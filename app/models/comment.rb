b12a3b7e04bad1e019bfd3351b3b89cafeb00196
Sean Chavez <schavez0013@gmail.com>
2016-10-04 14:32:44 -0600

    Associations, validations and Nested Resources

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  belongs_to :user
+
+  validates :post_id, presence: true
+  validates :body, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  has_many :comments, dependent:  :destroy
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :body, presence: true
 end
